753a2e316624dbef5f8ec82adff4ae39c7a9afa1
jigme1968 <jigme1968@gmail.com>
2020-03-07 02:04:44 +0800

    Initial Commit

---
+++ nce/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  nce
+//
+//  Created by jk on 2020/3/7.
+//  Copyright © 2020 ssl. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
