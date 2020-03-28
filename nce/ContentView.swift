5e22608cdf65fe6e3d80d05cf3f9db825bd7b717
jigme1968 <jigme1968@gmail.com>
2020-03-28 11:16:55 +0800

    changed

--- nce/ContentView.swift
+++ nce/ContentView.swift
@@ -30,24 +30,68 @@ struct ButtonLabel: View {
 
 struct ContentView: View {
   @State var recording: Bool = false
-  @State var speech: String = ""
+  @State var speech: String = "" // recognized text
+  
+ // BEGIN SR_view_contentview2
+ var body: some View {
+     NavigationView {
+         VStack(alignment: .leading) {
+             if !speech.isEmpty {
+                 Text(speech)
+                     .font(.largeTitle)
+                     .lineLimit(nil)
+             } else {
+                 Text("Speech will go here...")
+                     .font(.largeTitle)
+                     .foregroundColor(.gray)
+                     .lineLimit(nil)
+             }
+
+             Spacer()
+
+             if recording {
+                 Button(action: stopRecording) {
+                     ButtonLabel("Stop Recording", background: .red)
+                 }
+             } else {
+                 Button(action: startRecording) {
+                     ButtonLabel("Start Recording", background: .blue)
+                 }
+             }
+         }.padding()
+         .navigationBarTitle(Text("SRDemo"), displayMode: .inline)
+     }
+ }
+ // END SR_view_contentview2
   private let recognizer: SpeechRecognizer
+  
   init() {
     guard let recognizer = SpeechRecognizer() else {
-      fatalError("something gone wrong")
+      fatalError("something went wrong...")
     }
     self.recognizer = recognizer
 
   }
-  var body: some View {
-      Button(action: {}) {
-      Text("Hold to Talk")
+  private func startRecording() {
+    self.recording = true
+    self.speech = ""
+    
+    recognizer.startRecording { result in
+      if let text = result {
+        self.speech = text
+      } else {
+        self.stopRecording()
       }
+    }
   }
-}
+  private func stopRecording(){
+    self.recording = false
+    recognizer.stopRecording()
+  }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+  }
 }
